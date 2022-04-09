3f3e91c68f901418321ac846959bb20508e16898
Greg Munro <gregmunro90@gmail.com>
2022-04-09 11:08:35 +0200

    logic success

--- app.js
+++ app.js
@@ -4,30 +4,53 @@ const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
 let computerChoice;
-
-//this first grabs the buttons, and then forEach button, addEventListener which listens first for a click,
-//with each click, the function takes an 'event' which is the click, this function takes the id (saved in a global variable as userChoice)
-//then we get the userChoiceDisplay and set the html to what has been selected by the user (r p or sciss)
+let result;
 
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice();
+    getResult();
   })
 );
 
 function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+  const randomNumber = Math.floor(Math.random() * 3) + 1; // or you can use possibleChoices.length
 
   if (randomNumber === 1) {
     computerChoice = "rock";
   }
   if (randomNumber === 2) {
-    computerChoice = "scisscors";
+    computerChoice = "scissors";
   }
   if (randomNumber === 3) {
     computerChoice = "paper";
   }
   computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw!";
+  }
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lost!";
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose!";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose!";
+  }
+  resultDisplay.innerHTML = result;
 }
