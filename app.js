b2b57b3dcd5c3b28baf3d8e322f84488d774f16c
Greg Munro <gregmunro90@gmail.com>
2022-04-09 10:46:56 +0200

    first func with comments

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-const computerChoice = document.getElementById("computer-choice");
-const userChoice = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
+const possibleChoice = document.querySelectorAll("button");
+let userChoice;
+
+//this first grabs the buttons, and then forEach button, addEventListener which listens first for a click,
+//with each click, the function takes an 'event' which is the click, this function takes the id (saved in a global variable as userChoice)
+//then we get the userChoiceDisplay and set the html to what has been selected by the user (r p or sciss)
+
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+  })
+);
